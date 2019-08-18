1b50a2b09b970142c31b953d835cd82962d41924
MarcosApps <35972748+MarcosApp@users.noreply.github.com>
2019-08-18 17:07:45 -0300

    Adicionando novas fotos

--- detection_faces.py
+++ detection_faces.py
@@ -5,7 +5,7 @@ imagem = cv2.imread('pessoas.jpg')
 classificador = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-deteccoes = classificador.detectMultiScale(imagemCinza, scaleFactor=1.09, minNeighbors=5, minSize=(30,30))
+deteccoes = classificador.detectMultiScale(imagemCinza, scaleFactor=1.1, minNeighbors=3)
 
 print(deteccoes)
 print(len(deteccoes))
